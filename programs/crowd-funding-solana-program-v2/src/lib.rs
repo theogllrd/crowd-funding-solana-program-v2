9e5fe25fc8a620460179d10cb6f15d05cdf7a0a6
Theo Gaillard <theogllrd@gmail.com>
2022-05-28 12:20:55 +0200

    solana program and tests done

--- programs/crowd-funding-solana-program-v2/src/lib.rs
+++ programs/crowd-funding-solana-program-v2/src/lib.rs
@@ -6,10 +6,71 @@ declare_id!("Go9AcEhLBcyF8i79tXPJRQJi2ETg8mKnSCxzVxpQejJP");
 pub mod crowd_funding_solana_program_v2 {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn create_campaign(
+        ctx: Context<CreateCampaign>,
+        name: String,
+        description: String,
+    ) -> Result<()> {
+        // We get all the account from the context.
+        let campaign: &mut Account<Campaign> = &mut ctx.accounts.campaign;
+        let author: &Signer = &ctx.accounts.author;
+
+        // Guarding against invalid data.
+        if name.chars().count() > 50 {
+            return Err(ErrorCode::NameTooLong.into());
+        }
+        if description.chars().count() > 280 {
+            return Err(ErrorCode::DescriptionTooLong.into());
+        }
+
+        // Set all the values in the campaign account.
+        campaign.author = *author.key;
+        campaign.name = name;
+        campaign.description = description;
+        campaign.amount_donated = 0;
         Ok(())
     }
 }
 
+// Definition of the context.
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct CreateCampaign<'info> {
+    #[account(init, payer = author, space = Campaign::LEN)]
+    pub campaign: Account<'info, Campaign>, // means this is an account of type Campaign.
+    #[account(mut)]
+    pub author: Signer<'info>, // the signer of the transaction.
+    pub system_program: Program<'info, System>, // the official System_Program.
+}
+
+// Definition of the structure of the campaign account.
+#[account]
+pub struct Campaign {
+    pub author: Pubkey,
+    pub name: String,
+    pub description: String,
+    pub amount_donated: u64,
+}
+
+// Usefull constants for sizing properties.
+const DISCRIMINATOR_LENGTH: usize = 8;
+const PUBLIC_KEY_LENGTH: usize = 32;
+const STRING_LENGTH_PREFIX: usize = 4; // Stores the size of the string.
+const MAX_NAME_LENGTH: usize = 50 * 4; // 50 chars max for the campaign name.
+const MAX_DESCRIPTION_LENGTH: usize = 280 * 4; // 280 chars max for the campaign description.
+const MAX_AMOUNT_DONATED_LENGTH: usize = 8;
+
+impl Campaign {
+    const LEN: usize = DISCRIMINATOR_LENGTH
+        + PUBLIC_KEY_LENGTH // Author.
+        + STRING_LENGTH_PREFIX + MAX_NAME_LENGTH // Name.
+        + STRING_LENGTH_PREFIX + MAX_DESCRIPTION_LENGTH // Description.
+        + MAX_AMOUNT_DONATED_LENGTH; // Amount donated.
+}
+
+#[error_code]
+pub enum ErrorCode {
+    #[msg("The provided name should be 50 characters long maximum.")]
+    NameTooLong,
+    #[msg("The provided description should be 280 characters long maximum.")]
+    DescriptionTooLong,
+}
--- tests/crowd-funding-solana-program-v2.ts
+++ tests/crowd-funding-solana-program-v2.ts
@@ -1,16 +1,168 @@
 import * as anchor from "@project-serum/anchor";
 import { Program } from "@project-serum/anchor";
 import { CrowdFundingSolanaProgramV2 } from "../target/types/crowd_funding_solana_program_v2";
+import * as assert from "assert";
+import * as bs58 from "bs58";
 
 describe("crowd-funding-solana-program-v2", () => {
+
   // Configure the client to use the local cluster.
   anchor.setProvider(anchor.AnchorProvider.env());
-
   const program = anchor.workspace.CrowdFundingSolanaProgramV2 as Program<CrowdFundingSolanaProgramV2>;
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+
+  it('can create a new campaign', async () => {
+    // We generate a new keypair for our campaign account.
+    const campaign = anchor.web3.Keypair.generate();
+
+    await program.rpc.createCampaign('Computer for my son', 'Help me get a brand new laptop for my son', {
+      accounts: {
+        campaign: campaign.publicKey,
+        author: program.provider.wallet.publicKey,
+        systemProgram: anchor.web3.SystemProgram.programId,
+      },
+      signers: [campaign],
+    });
+    // Fetch the account details of the created tweet.
+    const campaignAccount = await program.account.campaign.fetch(campaign.publicKey);
+
+    assert.equal(campaignAccount.author.toBase58(), program.provider.wallet.publicKey.toBase58());
+    assert.equal(campaignAccount.name, 'Computer for my son');
+    assert.equal(campaignAccount.description, 'Help me get a brand new laptop for my son');
+    assert.equal(campaignAccount.amountDonated, 0);
+  });
+
+
+  it('can create a new campaign without name', async () => {
+    // We generate a new keypair for our campaign account.
+    const campaign = anchor.web3.Keypair.generate();
+
+    await program.rpc.createCampaign('', 'Help me get a brand new laptop for my son', {
+      accounts: {
+        campaign: campaign.publicKey,
+        author: program.provider.wallet.publicKey,
+        systemProgram: anchor.web3.SystemProgram.programId,
+      },
+      signers: [campaign],
+    });
+    // Fetch the account details of the created tweet.
+    const campaignAccount = await program.account.campaign.fetch(campaign.publicKey);
+
+    assert.equal(campaignAccount.author.toBase58(), program.provider.wallet.publicKey.toBase58());
+    assert.equal(campaignAccount.name, '');
+    assert.equal(campaignAccount.description, 'Help me get a brand new laptop for my son');
+    assert.equal(campaignAccount.amountDonated, 0);
+  });
+
+
+  it('can create a new campaign from a different author', async () => {
+    // We generate another user and airdrop them some SOL.
+    const otherUser = anchor.web3.Keypair.generate();
+    const signature = await program.provider.connection.requestAirdrop(otherUser.publicKey, 1000000000);
+    await program.provider.connection.confirmTransaction(signature);
+
+    // We generate a new keypair for our campaign account.
+    const campaign = anchor.web3.Keypair.generate();
+
+    await program.rpc.createCampaign('Computer for my son', 'Help me get a brand new laptop for my son', {
+      accounts: {
+        campaign: campaign.publicKey,
+        author: otherUser.publicKey,
+        systemProgram: anchor.web3.SystemProgram.programId,
+      },
+      signers: [otherUser, campaign],
+    });
+    // Fetch the account details of the created tweet.
+    const campaignAccount = await program.account.campaign.fetch(campaign.publicKey);
+
+    assert.equal(campaignAccount.author.toBase58(), otherUser.publicKey.toBase58());
+    assert.equal(campaignAccount.name, 'Computer for my son');
+    assert.equal(campaignAccount.description, 'Help me get a brand new laptop for my son');
+    assert.equal(campaignAccount.amountDonated, 0);
+  });
+
+
+  it('cannot provide a name with more than 50 characters', async () => {
+    try {
+      const campaign = anchor.web3.Keypair.generate();
+      const nameWith51Chars = 'x'.repeat(51);
+      await program.rpc.createCampaign(nameWith51Chars, 'Help me get a brand new laptop for my son', {
+        accounts: {
+          campaign: campaign.publicKey,
+          author: program.provider.wallet.publicKey,
+          systemProgram: anchor.web3.SystemProgram.programId,
+        },
+        signers: [campaign],
+      });
+    } catch (error) {
+      assert.equal(error.error.errorMessage, 'The provided name should be 50 characters long maximum.');
+      return;
+    }
+    assert.fail('The instruction should have failed with a 51-character name.');
+
+
+  });
+
+
+  it('cannot provide a description with more than 280 characters', async () => {
+    try {
+      const campaign = anchor.web3.Keypair.generate();
+      const descriptionWith51Chars = 'y'.repeat(281);
+      await program.rpc.createCampaign('help my buy a new car', descriptionWith51Chars, {
+        accounts: {
+          campaign: campaign.publicKey,
+          author: program.provider.wallet.publicKey,
+          systemProgram: anchor.web3.SystemProgram.programId,
+        },
+        signers: [campaign],
+      });
+    } catch (error) {
+      assert.equal(error.error.errorMessage, 'The provided description should be 280 characters long maximum.');
+      return;
+    }
+    assert.fail('The instruction should have failed with a 281-character description.');
+
+
+  });
+
+
+  it('can fetch all campaigns', async () => {
+    const campaignAccounts = program.account.campaign.all();
+    assert.equal((await campaignAccounts).length, 3)
+  });
+
+
+  it('can filter campaigns by author', async () => {
+    const authorPublicKey = program.provider.wallet.publicKey;
+    const campaignAccounts = program.account.campaign.all([
+      {
+        memcmp: {
+          offset: 8, // author data in campaignAccount starts at byte 8.
+          bytes: authorPublicKey.toBase58(),
+        }
+      }
+    ]);
+    assert.equal((await campaignAccounts).length, 2);
+    assert.ok((await campaignAccounts).every(campaignAccount => {
+      return campaignAccount.account.author.toBase58() == authorPublicKey.toBase58()
+    }))
+  });
+
+
+  it('can filter campaigns by name', async () => {
+    const campaignAccounts = program.account.campaign.all([
+      {
+        memcmp: {
+          offset: 8 + // Discriminator.
+            32 + // Author public key.
+            4, // Topic string prefix.
+          bytes: bs58.encode(Buffer.from('Computer for my son')),
+        }
+      }
+    ]);
+    assert.equal((await campaignAccounts).length, 2);
+    assert.ok((await campaignAccounts).every(campaign => {
+      return campaign.account.name === 'Computer for my son'
+    }));
   });
 });
